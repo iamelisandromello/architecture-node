d8945c419e6c0a79b6c02858a374c97e37c1cd4f
Elisandro Mello <elisandro@ulbra.edu.br>
2021-01-27 16:45:53 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, HttpResponse } from '../interfaces/http'
 import { MissinParmError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -7,10 +8,7 @@ export class SignUpController {
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new MissinParmError(field)
-        }
+        return badRequest(new MissinParmError(field))
       }
     }
   }
