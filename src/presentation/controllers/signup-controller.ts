97134928f01d10cf4ded1914e86e70c8d7a4a96f
Elisandro Mello <elisandro@ulbra.edu.br>
2021-01-27 14:49:30 -0300

    refactor: code optimization dynamic field verification

---
+++ src/presentation/controllers/signup-controller.ts
@@ -0,0 +1,14 @@
+export class SignUpController {
+  handle (httpRequest: any): any {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        }
+      }
+    }
+  }
+}
