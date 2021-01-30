d9a5b44f8e3c311077a2b07511976593ec19c925
Elisandro Mello <elisandro@ulbra.edu.br>
2021-01-30 13:04:31 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usescases/add-account/db-add-account.spec.ts
+++ src/data/usescases/add-account/db-add-account.spec.ts
@@ -39,4 +39,18 @@ describe('DbAddAccount Usecase', () => {
 
     expect(encryptSpy).toHaveBeenLastCalledWith('valid_password')
   })
+
+  test('Should throw if Encrypter throws ', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(Promise.reject(new Error()))
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+
+    await expect(promise).rejects.toThrow()
+  })
 })
