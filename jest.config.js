8ad780f55124117c82c2f7f1eff5001bdf27af6c
Elisandro Mello <elisandro@ulbra.edu.br>
2021-01-30 18:28:37 -0300

    chore: add exceptions for test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   roots: ['<rootDir>/src/'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-dependencies.ts',
+    '!**/interfaces/**',
+    '!**/test/**',
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
-  }
+    '@/(.*)': '<rootDir>/src/$1',
+  },
 }
