882445a3eff719f44f7ed05a7b147c67e044ad33
Elisandro Mello <elisandro@ulbra.edu.br>
2021-01-30 12:46:56 -0300

    refactor: move encrypter creation to a factory method

--- src/data/usescases/add-account/db-add-account.spec.ts
+++ src/data/usescases/add-account/db-add-account.spec.ts
@@ -1,15 +1,33 @@
 import { DbAddAccount } from '../add-account/db-add-account'
+import { Encrypter } from '../../interfaces/encrypter'
+
+const makeEcrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return Promise.resolve('hashed_password')
+    }
+  }
+
+  return new EncrypterStub()
+}
+
+type SutTypes = {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEcrypter()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password ', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return Promise.resolve('hashed_password')
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
     const accountData = {
