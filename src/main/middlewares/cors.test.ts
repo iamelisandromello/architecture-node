c00f179c52046d2cc9d0924d89097a9e6a3388d7
Elisandro Mello <elisandro@ulbra.edu.br>
2021-01-30 21:21:58 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,15 @@
+import app from '../config/app'
+import request from 'supertest'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_cors')
+      .expect('access-conttrol-allow-origin', '*')
+      .expect('access-conttrol-allow-methods', '*')
+      .expect('access-conttrol-allow-headers', '*')
+  })
+})
